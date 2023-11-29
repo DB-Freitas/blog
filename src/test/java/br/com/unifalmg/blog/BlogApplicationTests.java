ced7274a276555233d1cf124122b1bc11564521e
Douglas Brito Freitas <117559274+DB-Freitas@users.noreply.github.com>
2023-11-29 12:10:55 -0300

    add tests for metod get all users

--- src/test/java/br/com/unifalmg/blog/BlogApplicationTests.java
+++ src/test/java/br/com/unifalmg/blog/BlogApplicationTests.java
@@ -10,4 +10,26 @@ class BlogApplicationTests {
     void contextLoads() {
     }
 
+    
+    @Test
+    public void testGetAllUsers() {
+        // Criar uma lista de usuários fictícia para simular a resposta do repositório
+        List<User> userList = Arrays.asList(
+                new User(1, "John Doe", "john.doe", "john@example.com"),
+                new User(2, "Jane Doe", "jane.doe", "jane@example.com")
+        );
+
+        // Configurar o comportamento do repositório mock
+        when(userRepository.findAll()).thenReturn(userList);
+
+        // Chamar o método que queremos testar
+        List<User> result = userService.getAllUsers();
+
+        // Verificar se o método do repositório foi chamado
+        verify(userRepository, times(1)).findAll();
+
+        // Verificar se a lista retornada pelo serviço é a mesma que configuramos no mock
+        assertEquals(userList, result);
+    }
+
 }
